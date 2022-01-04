016ab913385bde2183a57a23c39f4dabc7deff0e
websummary <Morgenholt86@gmail.com>
2022-01-04 19:16:27 +0500

    feat: decided task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    return String(n = -n).split("").reverse().join("");
+  } else {
+    return String(n).split("").reverse().join("");
+  }
 }
